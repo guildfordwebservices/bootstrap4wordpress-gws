648c222ddd506a1d25cf8c1e9fe78abbfdb8aa5e
T-Jay <52924431+guildfordwebservices@users.noreply.github.com>
2021-01-06 20:56:13 +0000

    PHP title fix
    When using Yoast, the title appears twice, the new PHP fixes this bug

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
     <title>
-        <?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?>
+       <?php wp_title('-', true, 'right'); ?></title><span class="redactor-invisible-space"></span>
     </title>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
